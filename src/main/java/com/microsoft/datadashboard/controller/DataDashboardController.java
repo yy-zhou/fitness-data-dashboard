5f710e64ae68a5c804b6907071ba6c608a021395
Null <borismirage00@gmail.com>
2019-10-20 15:24:07 -0700

    2019-10-20 stage 1

--- src/main/java/com/microsoft/datadashboard/controller/DataDashboardController.java
+++ src/main/java/com/microsoft/datadashboard/controller/DataDashboardController.java
@@ -1,11 +1,39 @@
 package com.microsoft.datadashboard.controller;
 
+import org.springframework.web.bind.annotation.*;
+
 /**
+ * Controller.
+ *
  * @author BorisMirage
  * Time: 2019/10/20 14:54
  * Created with IntelliJ IDEA
  */
 
+@RestController             // bind as a controller, equals as @Controller + @ResponseBody
 public class DataDashboardController {
+    /**
+     * Hello world.
+     *
+     * @return hello message
+     */
+//    @RequestMapping(method = RequestMethod.GET)     // bind a router to current controller
+    @GetMapping
+//    @ResponseBody       // controller directly send response back to client as JSON
+    public String hello() {
+        return "Checking the main page";
+    }
 
+    /**
+     * Dynamic render the input message to browser.
+     *
+     * @param name given parameter
+     * @return welcome message
+     */
+//    @RequestMapping(value = "hello/{name}", method = RequestMethod.GET)
+    @GetMapping("hello/{name}")
+//    @ResponseBody
+    public String welcome(@PathVariable String name) {
+        return "Welcome back, " + name;
+    }
 }
