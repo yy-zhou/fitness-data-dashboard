562d7a4cc881638c096ddcca8bb44ccef022bcd1
Null <borismirage00@gmail.com>
2019-10-20 14:56:02 -0700

    Init commit

--- src/main/java/com/microsoft/datadashboard/DataDashboardApplication.java
+++ src/main/java/com/microsoft/datadashboard/DataDashboardApplication.java
@@ -8,6 +8,6 @@ public class DataDashboardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DataDashboardApplication.class, args);
-    }
 
+    }
 }
---
+++ src/main/java/com/microsoft/datadashboard/controller/DataDashboardController.java
@@ -0,0 +1,11 @@
+package com.microsoft.datadashboard.controller;
+
+/**
+ * @author BorisMirage
+ * Time: 2019/10/20 14:54
+ * Created with IntelliJ IDEA
+ */
+
+public class DataDashboardController {
+
+}
