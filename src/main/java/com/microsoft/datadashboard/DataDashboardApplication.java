4a364fbb03fb1f2c72665d122ae71eed2f3b984a
Null <borismirage00@gmail.com>
2019-10-27 15:15:51 -0700

    2019-10-27 Data JPA Auditing

--- src/main/java/com/microsoft/datadashboard/DataDashboardApplication.java
+++ src/main/java/com/microsoft/datadashboard/DataDashboardApplication.java
@@ -2,12 +2,13 @@ package com.microsoft.datadashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DataDashboardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DataDashboardApplication.class, args);
-
     }
 }
