2530519336d9f8dd5373124d56e4e721f5ed8e84
Null <borismirage00@gmail.com>
2019-10-27 15:15:26 -0700

    2019-10-27 Add data JPA pattern

--- src/main/java/com/microsoft/datadashboard/model/dvo/DataDVO.java
+++ src/main/java/com/microsoft/datadashboard/model/dvo/DataDVO.java
@@ -1,7 +1,21 @@
 package com.microsoft.datadashboard.model.dvo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import java.io.Serializable;
+import java.util.Date;
+
 /**
- * Data Value Object (DVO).
+ * Data Value Object (DVO) with Java Persistence API (JPA).
+ * Convert Java class to MySQL table.
  * Fitness tracking.
  *
  * @author BorisMirage
@@ -9,13 +23,38 @@ package com.microsoft.datadashboard.model.dvo;
  * Created with IntelliJ IDEA
  */
 
-public class DataDVO {
+@Entity
+@Table(name = "data")
+// ignore front end pass data for security
+@JsonIgnoreProperties(value = {"id", "createdAt", "updatedAt"}, allowGetters = true)
+@EntityListeners(AuditingEntityListener.class)      //
+public class DataDVO implements Serializable {      // serialize class to database-readable file
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     // auto increment
     private Long id;
-    private Long clientId;          // client, device id
-    private Double temperature;
-//    private Double bloodPressure;
-    private Integer heartBeat;
-    private Integer stepCount;
+
+    @NotNull
+    private Long clientId;      // client should not be null
+
+    @NotNull
+    @Min(20)
+    @Max(100)
+    private Double temperature;     // temperature should not be null, and it will have limit
+
+    @NotNull
+    @PositiveOrZero
+    private Integer stepCount;      // step count should be non-negative int value
+
+    @Column(nullable = false, updatable = false)        //
+    @Temporal(TemporalType.TIMESTAMP)                   // time stamp
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
 
     public Long getId() {
         return id;
@@ -41,19 +80,27 @@ public class DataDVO {
         this.temperature = temperature;
     }
 
-    public Integer getHeartBeat() {
-        return heartBeat;
-    }
-
-    public void setHeartBeat(Integer heartBeat) {
-        this.heartBeat = heartBeat;
-    }
-
     public Integer getStepCount() {
         return stepCount;
     }
 
     public void setStepCount(Integer stepCount) {
         this.stepCount = stepCount;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
     }
 }
