cae78b05a2687968392c9f347d8720bd3d97e22a
Null <borismirage00@gmail.com>
2019-12-21 13:47:49 -0800

    feat: add data simulator service module

---
+++ data-simulator/src/main/java/com/mirage/datasimulator/DataSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.mirage.datasimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSimulatorApplication.class, args);
+	}
+
+}
---
+++ data-simulator/src/test/java/com/mirage/datasimulator/DataSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirage.datasimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
