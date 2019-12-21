bf9d784e66fbb21057a5f5e1ae05b11cffcb232c
Null <borismirage00@gmail.com>
2019-12-21 13:39:38 -0800

    feat: add health monitor data model

---
+++ data-dashboard/src/main/java/com/mirage/datadashboard/Model/HealthMonitorData.java
@@ -0,0 +1,18 @@
+package com.mirage.datadashboard.Model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author BorisMirage
+ * Time: 2019/12/21 13:30
+ * Created with IntelliJ IDEA
+ */
+
+public class HealthMonitorData implements Serializable {
+    private Double temperature;
+    private Double bloodPressure;
+    private Integer heartBeat;
+    private Integer stepCount;
+    private Date timestamp;
+}
