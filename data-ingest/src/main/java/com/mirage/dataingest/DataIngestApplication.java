ec8ab91b7dc559c88e2d3260d1dac9f2a4d74a8c
Null <borismirage00@gmail.com>
2019-12-21 13:40:50 -0800

    feat: add data ingest service module

---
+++ data-ingest/src/main/java/com/mirage/dataingest/DataIngestApplication.java
@@ -0,0 +1,13 @@
+package com.mirage.dataingest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataIngestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataIngestApplication.class, args);
+	}
+
+}
---
+++ data-ingest/src/test/java/com/mirage/dataingest/DataIngestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirage.dataingest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataIngestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
