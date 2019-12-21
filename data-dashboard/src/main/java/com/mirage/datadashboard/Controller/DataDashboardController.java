61fdac6d70549ba3f4d7f898751f11b06858465c
Null <borismirage00@gmail.com>
2019-12-21 13:39:24 -0800

    feat: add data dashboard controller

---
+++ data-dashboard/src/main/java/com/mirage/datadashboard/Controller/DataDashboardController.java
@@ -0,0 +1,18 @@
+package com.mirage.datadashboard.Controller;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author BorisMirage
+ * Time: 2019/12/21 13:21
+ * Created with IntelliJ IDEA
+ */
+
+@Controller
+public class DataDashboardController {
+    private static final Logger logger = LoggerFactory.getLogger(DataDashboardController.class);
+}
