0ef26199c001fdc33c9a10739c84d594a4eb92bd
Null <borismirage00@gmail.com>
2019-12-21 13:38:12 -0800

    feat: add data dashboard service module

---
+++ data-dashboard/src/test/java/com/mirage/datadashboard/DataDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirage.datadashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
