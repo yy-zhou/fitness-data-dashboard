38c2e65a53a6a7518953269aad826dc06986b709
Null <borismirage00@gmail.com>
2019-12-22 13:14:11 -0800

    feat: add Netflix-Eureka service

--- service-registry/src/main/java/com/mirage/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/mirage/serviceregistry/ServiceRegistryApplication.java
@@ -2,8 +2,11 @@ package com.mirage.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
     public static void main(String[] args) {
