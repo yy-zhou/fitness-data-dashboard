0c3a783b4f11bb3a95c54b355a45b4bde60ab5a0
Null <borismirage00@gmail.com>
2019-12-30 16:54:45 -0800

    feat: add data consumer main application

--- data-consumer/src/test/java/com/mirage/dataconsumer/DataConsumerApplicationTests.java
+++ data-consumer/src/test/java/com/mirage/dataconsumer/DataConsumerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DataConsumerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
