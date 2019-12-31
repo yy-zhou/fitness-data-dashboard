658f89e78f7c9eef844712ff9a9005d61400e340
Null <borismirage00@gmail.com>
2019-12-30 21:53:13 -0800

    docs: add description

--- data-dashboard/src/main/java/com/mirage/datadashboard/DataDashboardApplication.java
+++ data-dashboard/src/main/java/com/mirage/datadashboard/DataDashboardApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * Main application for data dashboard.
+ */
 @SpringBootApplication
 @EnableRabbit
 @EnableDiscoveryClient
