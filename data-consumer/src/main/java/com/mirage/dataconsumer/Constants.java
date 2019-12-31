7b2d57b3368981fd4afb5da48a1b2565b7b5c533
Null <borismirage00@gmail.com>
2019-12-30 17:39:26 -0800

    feat: add constant strings to data consumer service

---
+++ data-consumer/src/main/java/com/mirage/dataconsumer/Constants.java
@@ -0,0 +1,17 @@
+package com.mirage.dataconsumer;
+
+/**
+ * @author BorisMirage
+ * Time: 2019/12/30 17:31
+ * Created with IntelliJ IDEA
+ */
+
+public class Constants {
+    public static final String EXCHANGE_NAME = "FitnessDataExchange";
+
+    public static final String QUEUE_NAME = "FitnessDataQueue";
+
+    public static final String ROUTING_KEY_HISTORY = "fitnessdata.history";
+
+    public static final String ROUTING_KEY_REALTIME = "fitnessdata.realtime";
+}
