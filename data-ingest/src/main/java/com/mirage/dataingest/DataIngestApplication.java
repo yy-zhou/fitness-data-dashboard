58db1de68a4e50242846e31681f163e7d8a64d2c
Null <borismirage00@gmail.com>
2019-12-30 16:54:14 -0800

    feat: add data ingest main application

--- data-ingest/src/main/java/com/mirage/dataingest/DataIngestApplication.java
+++ data-ingest/src/main/java/com/mirage/dataingest/DataIngestApplication.java
@@ -1,13 +1,24 @@
 package com.mirage.dataingest;
 
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableRabbit
 public class DataIngestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DataIngestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DataIngestApplication.class, args);
+    }
 
+    @Bean
+    public Exchange dataExchange() {
+        return new TopicExchange(Constants.EXCHANGE_NAME);
+    }
 }
