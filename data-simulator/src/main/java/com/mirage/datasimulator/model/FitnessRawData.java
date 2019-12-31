0aec5d2f604792a35bcaf75d43f42a3157b4d079
Null <borismirage00@gmail.com>
2019-12-30 16:46:46 -0800

    feat: add raw data format for simulator to simulate data

---
+++ data-simulator/src/main/java/com/mirage/datasimulator/model/FitnessRawData.java
@@ -0,0 +1,24 @@
+package com.mirage.datasimulator.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * Raw data format for testing.
+ *
+ * @author BorisMirage
+ * Time: 2019/12/22 12:44
+ * Created with IntelliJ IDEA
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FitnessRawData implements Serializable {
+    private Double temperature;
+    private Double bloodPressure;
+    private Integer heartBeat;
+    private Integer stepCount;
+}
