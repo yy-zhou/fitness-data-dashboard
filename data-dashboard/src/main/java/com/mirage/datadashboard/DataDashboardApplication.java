912cd8b17e358d3c09fe144dbdf06b516886e9a3
Null <borismirage00@gmail.com>
2019-12-30 18:10:30 -0800

    feat: add main application to data dashboard service

--- data-dashboard/src/main/java/com/mirage/datadashboard/DataDashboardApplication.java
+++ data-dashboard/src/main/java/com/mirage/datadashboard/DataDashboardApplication.java
@@ -1,13 +1,36 @@
 package com.mirage.datadashboard;
 
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableRabbit
+@EnableDiscoveryClient
 public class DataDashboardApplication {
+
+    @Bean
+    public Exchange fitnessDataExchange() {
+        return new TopicExchange(Constants.EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Queue queue() {
+        return new Queue(Constants.QUEUE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue q, Exchange dataExchange) {
+        return BindingBuilder
+                .bind(q)
+                .to(dataExchange)
+                .with(Constants.ROUTING_KEY_REALTIME).noargs();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DataDashboardApplication.class, args);
     }
-
 }
