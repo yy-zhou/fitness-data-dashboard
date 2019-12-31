5a799a254e18a7cb980c84357c525fd9fa524f80
Null <borismirage00@gmail.com>
2019-12-30 17:00:28 -0800

    feat: add Eureka and Zuul used in API gateway

--- api-gateway/src/main/java/com/mirage/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/mirage/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,12 @@ package com.mirage.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
