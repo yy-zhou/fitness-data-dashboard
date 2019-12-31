da871ad584510ac4507d492c977b0c44d888ddd2
Null <borismirage00@gmail.com>
2019-12-30 18:09:23 -0800

    feat: override WebSocket config

---
+++ data-dashboard/src/main/java/com/mirage/datadashboard/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.mirage.datadashboard.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+/**
+ * @author BorisMirage
+ * Time: 2019/12/30 17:48
+ * Created with IntelliJ IDEA
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry messageBrokerRegistry) {
+        messageBrokerRegistry.setApplicationDestinationPrefixes("/app").enableSimpleBroker("/topic");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket-app").withSockJS();
+    }
+}
