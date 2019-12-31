9858acc7d77606bb808607a360e73f7ce6750b63
Null <borismirage00@gmail.com>
2019-12-30 21:42:17 -0800

    docs: add description

--- data-consumer/src/main/java/com/mirage/dataconsumer/Constants.java
+++ data-consumer/src/main/java/com/mirage/dataconsumer/Constants.java
@@ -1,6 +1,12 @@
 package com.mirage.dataconsumer;
 
 /**
+ * Definition of constant in project.
+ * The following fields was defined:
+ * 1. Exchange name for data exchange
+ * 2. Data queue name for RabbitMQ
+ * 3. Routing key for history consumer (database)
+ * 4. Routing key for real time display consumer
  * @author BorisMirage
  * Time: 2019/12/30 17:31
  * Created with IntelliJ IDEA
