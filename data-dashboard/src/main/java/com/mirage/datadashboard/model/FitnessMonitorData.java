cfe0b36c2fcab387b8807bf8fd0a138ed391fa7f
Null <borismirage00@gmail.com>
2019-12-30 17:18:49 -0800

    feat: convert data type to be fitted in database

--- data-dashboard/src/main/java/com/mirage/datadashboard/model/FitnessMonitorData.java
+++ data-dashboard/src/main/java/com/mirage/datadashboard/model/FitnessMonitorData.java
@@ -1,5 +1,10 @@
 package com.mirage.datadashboard.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -8,8 +13,11 @@ import java.util.Date;
  * Time: 2019/12/21 13:30
  * Created with IntelliJ IDEA
  */
-
-public class HealthMonitorData implements Serializable {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collation = "healthdata")
+public class FitnessMonitorData implements Serializable {
     private Double temperature;
     private Double bloodPressure;
     private Integer heartBeat;
