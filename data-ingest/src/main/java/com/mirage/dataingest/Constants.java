07e8fb444c98da2b4c42295e6454739f3aebc623
Null <borismirage00@gmail.com>
2019-12-30 16:52:54 -0800

    feat: add definition of constants

---
+++ data-ingest/src/main/java/com/mirage/dataingest/Constants.java
@@ -0,0 +1,18 @@
+package com.mirage.dataingest;
+
+/**
+ * @author BorisMirage
+ * Time: 2019/12/23 16:51
+ * Created with IntelliJ IDEA
+ */
+
+public class Constants {
+
+    public static final String EXCHANGE_NAME = "FitnessDataExchange";
+
+    public static final String QUEUE_NAME = "FitnessDataQueue";
+
+    public static final String ROUTING_KEY_HISTORY = "fitnessdata.history";
+
+    public static final String ROUTING_KEY_REALTIME = "fitnessdata.realtime";
+}
